7ca4b8d211331946500857906e11b27dd32931f4
JefersonOC <dev.blackfishlabs@gmail.com>
2020-10-24 08:58:46 -0400

    [release] reformat code

--- forza-service/src/main/java/io/github/blackfishlabs/forza/core/application/receita/resource/ReceitaWSResource.java
+++ forza-service/src/main/java/io/github/blackfishlabs/forza/core/application/receita/resource/ReceitaWSResource.java
@@ -4,7 +4,10 @@ import io.github.blackfishlabs.forza.core.application.receita.json.DadosReceitaJ
 import io.github.blackfishlabs.forza.core.application.receita.service.ReceitaWSService;
 import io.github.blackfishlabs.forza.core.infra.resource.APIMap;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/receita")
