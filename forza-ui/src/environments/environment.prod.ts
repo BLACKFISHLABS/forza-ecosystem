b65c28d6517a15541ec0151c7305c214f15a2312
jeferson <dev.blackfishlabs@gmail.com>
2021-04-10 11:15:37 -0400

    ip production

--- forza-ui/src/environments/environment.prod.ts
+++ forza-ui/src/environments/environment.prod.ts
@@ -1,8 +1,6 @@
 export const environment = {
   production: true,
   // ! important production
-  // apiUrl: 'http://forza.blackfishlabs.com.br/api/v1',
-  // apiUrlPublic: 'http://forza.blackfishlabs.com.br/public',
-  apiUrl: 'http://157.245.168.143:5000/api/v1',
-  apiUrlPublic: 'http://157.245.168.143:5000/public',
+  apiUrl: 'http://http://167.172.220.137:5050//api/v1',
+  apiUrlPublic: 'http://http://167.172.220.137:5050/public',
 };
